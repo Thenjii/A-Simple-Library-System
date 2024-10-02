a5f18675f620fadc654835ef07d0ca79cc78b927
Thenjiwe <thenjiwelamani15@gmail.com>
2024-10-02 15:03:46 +0200

    donzo

--- scripts.js
+++ scripts.js
@@ -1,32 +1,32 @@
 //book constructor
 function Book(title, author, year) {
-this.title = title;
-this.author = author;
-this.year = year;
+    this.title = title;
+    this.author = author;
+    this.year = year;
 }
 
 //author constructor
-function Author (name, last, books){
-    this.name = name;
-    this.last = last;
-    this.books = books;
+function Author (firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.books = [];
 
 }
 
 //method that adds Book to books
 Author.prototype.addBook = function(book){
-    this.books,push(book);
+    this.books.push(book);
 }
 
 // authors 
-let author1 = new Author("Douglas Crockford");
-let author2 = new Author("Jon Duckett");
-let author3 = new Author("David Flanagan");
+let author1 = new Author("Douglas", "Crockford");
+let author2 = new Author("Jon", "Duckett");
+let author3 = new Author("David", "Flanagan");
 
 //their books
-let book1 = new Book("JavaScript: The Good Parts", 2008);
-let book2 = new Book("JavaScript and JQuery: Interactive Front-End Web Development", 2013);
-let book3 = new Book("JavaScript: The Definitive Guide: Master the World's Most-Used Programming Language", 2020);
+let book1 = new Book("JavaScript: The Good Parts", "Douglas Crockford", 2008);
+let book2 = new Book("JavaScript and JQuery: Interactive Front-End Web Development", "Jon Duckett", 2013);
+let book3 = new Book("JavaScript: The Definitive Guide: Master the World's Most-Used Programming Language", "David Flanagan", 2020);
 
 // add books to authors
 author1.addBook(book1);
@@ -34,5 +34,16 @@ author2.addBook(book2);
 author3.addBook(book3);
 
 //authors and their books
-console.log("Library");
-console.log(${author1.name}'s books: author1.books);
+function displayAuthorsAndBooks(author){
+    console.log(`Author: ${author.firstName} ${author.lastName}`);
+    console.log("Books:");
+    author.books.forEach(function(book, index) {
+        console.log(`${index + 1}. ${book.title} (${book.year})`);
+    });
+    console.log("\n");
+}
+
+// Display each author and their books
+displayAuthorsAndBooks(author1);
+displayAuthorsAndBooks(author2);
+displayAuthorsAndBooks(author3);   
